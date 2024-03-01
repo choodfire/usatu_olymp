657f3a28dd93f33539cff58620216117fb344a60
Ruslan <khalitovr21@gmail.com>
2024-03-01 18:57:38 +0500

    Fix init db

--- database/database.py
+++ database/database.py
@@ -2,7 +2,29 @@ from pymysql import Connection
 
 
 def init_db(db: Connection) -> None:
-    with open('database/schema.sql', encoding='utf-8') as f:
-        command = f.read()
-        with db.cursor() as cursor:
-            cursor.execute(command)
+    with open('database/schema_users.sql', encoding='utf-8') as f_users:
+        sql_users = f_users.read()
+    with open('database/schema_payments.sql', encoding='utf-8') as f_pensions:
+        sql_pensions = f_pensions.read()
+    with db.cursor() as cursor:
+        cursor.execute(sql_users)
+        cursor.execute(sql_pensions)
+
+
+def select_all_users_payments(db: Connection) -> tuple[str, str, float]:
+    sql = ("SELECT users.id, users.fio, payments.type, payments.amount FROM users INNER JOIN payments ON "
+           "users.id=payments.user_id;")
+    with db.cursor() as cursor:
+        cursor.execute(sql)
+    result = cursor.fetchall()
+
+    return result
+
+
+def select_all_users(db: Connection) -> tuple[str, str]:
+    sql = ("SELECT users.id, users.fio FROM users")
+    with db.cursor() as cursor:
+        cursor.execute(sql)
+    result = cursor.fetchall()
+
+    return result
---
+++ database/schema_payments.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS payments (
+    pension_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    amount FLOAT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
