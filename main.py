fddce7231f29e5ab56f37ef13f451b6c5c9e1007
Ruslan <khalitovr21@gmail.com>
2024-03-01 16:59:54 +0500

    Env file and template fix

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
 import pymysql
+from dotenv import load_dotenv
+import os
 
+env_path = '.env'
+load_dotenv(dotenv_path=env_path)
 app = Flask(__name__)
-db = pymysql.connect(host="localhost", user="root", password="pass", database="db")
+
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_DATABASE = os.getenv('DB_DATABASE')
+db = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASS, database=DB_DATABASE)
 
 
 @app.route('/')
